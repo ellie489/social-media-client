516ff6a2e9e05ac768420a3538a22a3f577658c3
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-11-10 10:23:47 +0100

    Updates e2e cypress login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,13 @@
-describe("Social Media App: Existing User", () => {
+describe("Social Media App: Login", () => {
   beforeEach(() => {
     cy.visit("https://ellie489.github.io/social-media-client/");
     cy.clearLocalStorage();
-
-    // Click on the login button within the register form
     cy.wait(1000);
+
     cy.get("#registerModal").contains("Login").click();
 
-    // Wait for the login form to appear
     cy.get("#loginForm").should("be.visible");
 
-    // Type email and password
     cy.get("#loginForm #loginEmail").should("exist").click();
     cy.wait(1000);
     cy.get("#loginForm #loginEmail")
@@ -21,23 +18,32 @@ describe("Social Media App: Existing User", () => {
       .should("exist")
       .type("Testing123{enter}");
   });
+
   it("Can login and check token exist in localstorage", () => {
     cy.get("button[type=submit]").contains("Login").click();
-    cy.wait(1000);
+    cy.url().should("include", "?view=profile");
 
-    cy.window().then((win) => {
-      const token = win.localStorage.getItem("token");
-      cy.log("Token from local storage:", token);
+    //     cy.window().then((win) => {
+    //       const token = win.localStorage.getItem("token");
+    //       cy.log("Token from local storage:", token);
 
-      cy.request({
-        method: "GET",
-        url: "https://ellie489.github.io/social-media-client/?view=profile&name=Testingelisabeth",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }).then((response) => {
-        cy.log("API Response:", response);
-      });
-    });
+    //       cy.request({
+    //         method: "GET",
+    //         url: "https://ellie489.github.io/social-media-client/?view=profile&name=Testingelisabeth",
+    //         headers: {
+    //           Authorization: `Bearer ${token}`,
+    //         },
+    //       }).then((response) => {
+    //         cy.log("API Response:", response);
+    //       });
+    //     });
   });
 });
+// describe("Logout Feature", () => {
+//     it("Logs out successfully", () => {
+
+//       cy.get("button[data-test=logout-button]").click();
+//       cy.url().should("include", "/login");
+
+//     });
+//   });
