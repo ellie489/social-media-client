6187a7485861e89ba00331b2a3de08303f678c8e
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-11-15 13:15:19 +0100

    Adds login test for valid user

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe("Social Media App: Login", () => {
   beforeEach(() => {
-    cy.visit("https://ellie489.github.io/social-media-client/");
+    cy.visit("/");
     cy.clearLocalStorage();
     cy.wait(1000);
 
@@ -22,28 +22,5 @@ describe("Social Media App: Login", () => {
   it("Can login and check token exist in localstorage", () => {
     cy.get("button[type=submit]").contains("Login").click();
     cy.url().should("include", "?view=profile");
-
-    //     cy.window().then((win) => {
-    //       const token = win.localStorage.getItem("token");
-    //       cy.log("Token from local storage:", token);
-
-    //       cy.request({
-    //         method: "GET",
-    //         url: "https://ellie489.github.io/social-media-client/?view=profile&name=Testingelisabeth",
-    //         headers: {
-    //           Authorization: `Bearer ${token}`,
-    //         },
-    //       }).then((response) => {
-    //         cy.log("API Response:", response);
-    //       });
-    //     });
   });
 });
-// describe("Logout Feature", () => {
-//     it("Logs out successfully", () => {
-
-//       cy.get("button[data-test=logout-button]").click();
-//       cy.url().should("include", "/login");
-
-//     });
-//   });
